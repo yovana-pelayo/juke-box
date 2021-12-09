ae475002fc23e87c72221252d78eb1eeb1d91f4d
Yovana Pelayo <yovana_pelayo@yahoo.com>
2021-12-08 16:57:30 -0800

    added data to list

---
+++ data.js
@@ -0,0 +1,14 @@
+export const figures = [{
+  name: 'Devil',
+  image: 'assets/avatars/devils.png',
+  audio: 'assets/sounds/devil.mp3'
+
+}, {
+  name: 'Thor',
+  image: 'assets/avatars/thors.png',
+  audio: 'assets/sounds/thor.mp3'
+}, {
+  name: 'Princess',
+  image: 'assets/avatars/princessB.png',
+  audio: 'assets/sounds/princessB.mp3'
+}];
